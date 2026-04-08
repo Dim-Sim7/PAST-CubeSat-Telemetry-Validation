203a013b6236b72503a689bf016ce1f1410692a7
Dimitrije Simic <dimitrijesmc@gmail.com>
2026-04-08 18:33:30 +0800

    refactored ring buffer. Implemented FreeRTOS producer consumer tasks

--- transmitter/Src/telemetry.h
+++ transmitter/Src/telemetry.h
@@ -12,8 +12,9 @@
 
 #define MAX_PAYLOAD_SIZE 32 //Maximum bytes can be stored in payload
 #define MAX_PACKET_SIZE sizeof(TelemetryPacket_t)
-#define HISTORY_SIZE 128 // Can store a history of up to 128 packets at once
-#define HISTORY_MASK (HISTORY_SIZE - 1)
+
+
+#
 
 #define TELEMETRY_SOF 0xAA
 #define TELEMETRY_EOF 0x55
@@ -26,7 +27,7 @@ typedef struct __attribute__((__packed__))
 {
     uint8_t sof;
     uint8_t type;
-    uint16_t seq;
+    uint32_t seq;
     uint8_t reliable;
     uint8_t len;
     uint8_t payload[MAX_PAYLOAD_SIZE];
@@ -71,26 +72,9 @@ typedef struct __attribute__((__packed__))
 typedef struct __attribute__((__packed__))
 {
     uint8_t type;
-    uint16_t from_seq;
-    uint16_t to_seq;
+    uint32_t from_seq;
+    uint32_t to_seq;
 } RetransmitRequest;
-
-typedef struct __attribute__((__packed__))
-{
-    uint16_t size;                 // actual size of the stored packet
-    uint16_t seq;
-    uint8_t type;
-    uint8_t data[MAX_PACKET_SIZE]; // serialized TelemetryPacket_t
-} HistoryRingBufferEntry;
-
-typedef struct
-{
-    HistoryRingBufferEntry entries[HISTORY_SIZE];
-    uint16_t head;
-    uint16_t tail;
-    uint16_t count;
-} HistoryRingBuffer;
-
 
 /* Helper enums and struct to construct packets */
 typedef enum {
@@ -105,24 +89,29 @@ typedef enum {
     PACKET_TYPE_BATTERY
 } PacketType_e;
 
+
 typedef struct {
     PacketType_e type;
     uint8_t      len;
     Reliability_e reliable;
 } PacketInfo_t;
 
+/* Used to cycle between types and read data */
+static const PacketType_e PACKET_TYPES[] = {
+    PACKET_TYPE_GNSS,
+    PACKET_TYPE_BARO,
+    PACKET_TYPE_IMU,
+    PACKET_TYPE_BATTERY
+};
 /* Using designated initialiser to specify the enum as the index */
 static const PacketInfo_t PACKET_INFO[] = {
-    [PACKET_TYPE_GNSS]    = { PACKET_TYPE_GNSS, sizeof(GNSSData_t), RELIABLE        },
+    [PACKET_TYPE_GNSS]    = { PACKET_TYPE_GNSS, sizeof(GNSSData_t), UNRELIABLE      },
     [PACKET_TYPE_BARO]    = { PACKET_TYPE_BARO, sizeof(BarometerData_t), UNRELIABLE },
     [PACKET_TYPE_IMU]     = { PACKET_TYPE_IMU, sizeof(IMUData_t), UNRELIABLE        },
-    [PACKET_TYPE_BATTERY] = { PACKET_TYPE_BATTERY, sizeof(BatteryData_t), RELIABLE  },
+    [PACKET_TYPE_BATTERY] = { PACKET_TYPE_BATTERY, sizeof(BatteryData_t), RELIABLE  }
 };
 
 uint16_t calculateCrc(const uint8_t* data, uint8_t len);
-void createPacket(TelemetryPacket_t* packet, const void* data, PacketType_e type, uint16_t* cur_seq);
-HistoryRingBufferEntry* getFromHistorySpecific(HistoryRingBuffer* historyBuffer, int seq);
-HistoryRingBufferEntry* getFromHistory(HistoryRingBuffer* historyBuffer);
-void addToHistory(HistoryRingBuffer* historyBuffer, TelemetryPacket_t* packet);
+void createPacket(TelemetryPacket_t* packet, const void* data, PacketType_e type, uint32_t* cur_seq);
 
 #endif
